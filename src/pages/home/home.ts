36df3b1c55db801b7b6e058ac4ee0963301f48eb
Shubhrit-PC\Shubhrit <shubhritdhanawat1117@gmail.com>
2017-12-12 01:05:57 +0530

    infinite scroll added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { RestApiProvider } from './../../providers/rest-api/rest-api';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -7,8 +8,68 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  data= [];
+  users = [];
+  errorMessage: string;
+  page = 1;
+  perPage = 10;
+  totalPage = 0;
+  i = 0;
+  totalData: any;
+
+  constructor(public navCtrl: NavController, public restApi: RestApiProvider) {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.restApi.getUsers()
+      .subscribe(
+      res => {
+        this.totalData = res;
+        for(let c=0; c< 10; c++)
+        this.data.push(this.totalData[c]);
+        console.log(typeof this.data)
+      },
+      error => this.errorMessage = <any>error);
+  }
+
+  doInfinite(infiniteScroll) {
+    setTimeout(() => {
+      this.restApi.getUsers()
+        .subscribe(
+        res => {
+          this.totalData = res;
+
+          if (this.perPage < this.totalData.length) {
+            for (this.i = this.perPage; this.i < this.perPage + 10; this.i++) {
+              this.users.push(this.totalData[this.i]);
+            }
+            this.perPage = this.i;
+            console.log(this.perPage);
+          }
+        },
+        error => this.errorMessage = <any>error);
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+    }, 500);
 
   }
 
 }
+
+//   doInfinite(infiniteScroll): Promise<any> {
+//     console.log('Begin async operation');
+
+//     return new Promise((resolve) => {
+//       setTimeout(() => {
+//         for (var i = 0; i < 30; i++) {
+//           this.data.push(this.data.length);
+//         }
+//         console.log(this.data)
+
+//         console.log('Async operation has ended');
+//         resolve();
+//       }, 500);
+//     })
+//   }
+// }
